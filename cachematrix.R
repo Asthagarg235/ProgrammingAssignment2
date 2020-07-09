25daa3cfbc8f8de86e478c2be7e3dc3b8f738b92
Asthagarg235 <67836484+Asthagarg235@users.noreply.github.com>
2020-07-09 21:59:49 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function create a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
-        set <- function(y){
-                x <<- y
-                inv <<- NULL
+         inv <- NULL                       ## intialise inv as NULL
+        set <- function(y){                ##define the set function to assign new
+                x <<- y                    ##value of  matrix in parent environment
+                inv <<- NULL               ##if there is a new matrix then reset it as NULL
         }
-        get <- function() {x}
-        setInverse <- function(inverse) {inv <<- inverse}
-        getInverse <- function() {inv}
+        get <- function() {x}              ## define the get function
+        setInverse <- function(inverse) {inv <<- inverse}    ## assign the value of inv in parent environment
+        getInverse <- function() {inv}                       ## get the value of inv where called
         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        inv <- x$getInverse()
+cacheSolve <- function(x, ...) {   ##return a matrix that is inverse of x If the inverse has already been calculated (and the matrix has not changed),
+## then cacheSolve will retrieve the inverse from the cache
+        inv <- x$getInverse()     
         if(!is.null(inv)){
                 message("getting cached data")
                 return(inv)
@@ -28,5 +27,6 @@ cacheSolve <- function(x, ...) {
         inv <- solve(mat, ...)
         x$setInverse(inv)
         inv
-        ## Return a matrix that is the inverse of 'x'
+        
 }
+
